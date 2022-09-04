239b2de7fb5c71f554ac8c41c0be61adb3614052
arthurlba1 <1arthurluiz1@gmail.com>
2022-09-04 19:33:41 -0300

    removed unused code after optimizer

--- backend/chess_pieces/beans/__init__.py
+++
@@ -1,4 +0,0 @@
-"""Chess pieces app beans init module."""
-
-from chess_pieces.beans.chess_board_bean import ChessBoardBean
-from chess_pieces.beans.chess_pieces_bean import ChessPiecesBean
--- backend/chess_pieces/serializers/chess_pieces_serializer.py
+++ backend/chess_pieces/serializers/chess_pieces_serializer.py
@@ -1,12 +1,11 @@
 from rest_framework import serializers
 
 from chess_pieces.constants.model_constants import ChessPieceConstants
-from chess_pieces.beans import ChessPiecesBean
 from chess_pieces.models import ChessPiece
 
 
 class ChessPiecesSerializer(serializers.ModelSerializer):
-    """Serializes the chess board bean"""
+    """Serializes the chess board"""
     class Meta:
         model = ChessPiece
         fields = ['pk', 'name', 'color']
