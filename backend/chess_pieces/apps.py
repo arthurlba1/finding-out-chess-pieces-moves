5b14dbad15dbb35fe179d70f193d119a41793296
arthurlba1 <1arthurluiz1@gmail.com>
2022-09-04 01:29:30 -0300

    viewset build url

--- backend/chess_pieces/apps.py
+++ backend/chess_pieces/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class ChessPiecesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'chess_pieces'
+    verbose_name = "Chess Pieces"
--- backend/chess_pieces/urls.py
+++ backend/chess_pieces/urls.py
@@ -1,1 +1,7 @@
-from django.urls import path
+from chess_pieces.rests import ChessBoardViewSet
+from chess_pieces.rests import ChessPieceViewSet
+
+urlpatterns = [
+    ChessBoardViewSet.build_urls(),
+    ChessPieceViewSet.build_urls()
+]
